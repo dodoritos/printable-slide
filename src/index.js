673a92cf7a62e1cb828f3de0a2b4039b58c0d41c
Dorian <dorian_hoet@hotmail.com>
2022-11-21 15:09:52 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
+
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+async function createPdf() {
+  const pdfDoc = await PDFDocument.create()
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+
+  const page = pdfDoc.addPage()
+  const { width, height } = page.getSize()
+  const fontSize = 30
+  page.drawText('Creating PDFs in JavaScript is awesome!', {
+    x: 50,
+    y: height - 4 * fontSize,
+    size: fontSize,
+    font: timesRomanFont,
+    color: rgb(0, 0.53, 0.71),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+  // download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
+}
+document.body.appendChild(component());
+document.getElementById("btn").onclick = function() {
+  console.log("hello");
+  createPdf();
+};
