7aa4b13ac222436a0ed9f6b8dccab9409d9e3e90
Dorian <dorian_hoet@hotmail.com>
2022-11-21 18:41:28 +0100

    upload and download

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
+import download from "downloadjs";
 
 
 function component() {
@@ -27,11 +28,17 @@ async function createPdf() {
   })
 
   const pdfBytes = await pdfDoc.save()
-  // download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
+  download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
 }
 
 console.log("hello")
 document.body.appendChild(component());
+let input = document.createElement("input");
+input.type = "file"
+input.id = "pdf-upload"
+document.body.appendChild(input)
+
+
 let btn = document.createElement("button");
 btn.innerHTML = "Click Me";
 document.body.appendChild(btn);
@@ -39,3 +46,66 @@ btn.onclick = function() {
   console.log("hello");
   createPdf();
 };
+
+document.querySelector("#pdf-upload").addEventListener("change", function(e){
+  var canvasElement = document.querySelector("canvas")
+  var file = e.target.files[0]
+  if(file.type != "application/pdf"){
+    console.error(file.name, "is not a pdf file.")
+    return
+  }
+
+  var fileReader = new FileReader();
+
+  fileReader.onload = async function() {
+    var pdfBytes = new Uint8Array(this.result);
+    const usConstitutionPdf = await PDFDocument.load(pdfBytes);
+
+    const pdfOutBytes = await usConstitutionPdf.save();
+
+    download(pdfOutBytes, "pdf-upload.pdf", "application/pdf");
+  };
+
+  fileReader.readAsArrayBuffer(file);
+})
+
+
+async function embedPdfPages() {
+  const flagUrl = 'https://pdf-lib.js.org/assets/american_flag.pdf';
+  const constitutionUrl = 'https://pdf-lib.js.org/assets/us_constitution.pdf';
+
+  const flagPdfBytes = await fetch(flagUrl).then((res) => res.arrayBuffer());
+  const constitutionPdfBytes = await fetch(constitutionUrl).then((res) =>
+      res.arrayBuffer(),
+  );
+
+  const pdfDoc = await PDFDocument.create();
+
+  const [americanFlag] = await pdfDoc.embedPdf(flagPdfBytes);
+
+  const usConstitutionPdf = await PDFDocument.load(constitutionPdfBytes);
+  const preamble = await pdfDoc.embedPage(usConstitutionPdf.getPages()[1], {
+    left: 55,
+    bottom: 485,
+    right: 300,
+    top: 575,
+  });
+
+  const americanFlagDims = americanFlag.scale(0.3);
+  const preambleDims = preamble.scale(2.25);
+
+  const page = pdfDoc.addPage();
+
+  page.drawPage(americanFlag, {
+    ...americanFlagDims,
+    x: page.getWidth() / 2 - americanFlagDims.width / 2,
+    y: page.getHeight() - americanFlagDims.height - 150,
+  });
+  page.drawPage(preamble, {
+    ...preambleDims,
+    x: page.getWidth() / 2 - preambleDims.width / 2,
+    y: page.getHeight() / 2 - preambleDims.height / 2 - 50,
+  });
+
+  const pdfBytes = await pdfDoc.save();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
     }),
   ],
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'auto',
+    publicPath: '/',
+    clean: true,
   },
   devServer: {
     static: {
